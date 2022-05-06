3ed20cb402b802e6dae8be473982e6c86c01b2c4
Maxime Palmino <maximepalmino.pro@gmail.com>
2022-05-06 13:39:16 +0200

    [UPDATE]Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -126,16 +126,7 @@ const Header: React.FC = () => {
 
     return (
       <>
-      <div data-scroll-container>
-    <div data-scroll-section>
-        <h1 data-scroll>Hey</h1>
-        <p data-scroll>👋</p>
-    </div>
-    <div data-scroll-section>
-        <h2 data-scroll data-scroll-speed="1">What's up?</h2>
-        <p data-scroll data-scroll-speed="2">😬</p>
-    </div>
-</div>
+
       <div className={styles.container}>
         <motion.header className={styles.header}>
           {/* <Nav /> */}
